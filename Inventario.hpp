64945e9c6f6642ebd2ca8ecc878ee9c21bcdac9d
Raissa Lauar Navarro <raissalgns@gmail.com>
2023-11-07 21:39:04 -0300

    alterações de arquivos hpp

---
+++ Inventario.hpp
@@ -0,0 +1,9 @@
+#include <map>
+#include "Item.hpp"
+
+class Inventario{
+    public:
+
+        std::map<unsigned, Item> _inventario;
+    private:
+};
---
+++ Item.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Item{
+    public:
+
+    private:
+        int _codigo;
+        int _quantidade;
+        int _preco;
+        bool _status;
+        std::string _nome;
+};
