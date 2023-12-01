dd9bd4730fb7d9fb24869de887f347404d7f173a
Raissa Lauar Navarro <raissalgns@gmail.com>
2023-12-01 11:37:39 -0300

    alterações da Naually
    juntando todos os arquivos na mesma branch

--- Item.hpp
+++ Item.hpp
@@ -5,19 +5,34 @@
 class Item
 {
 public:
+    
+    /// @brief Construtor da classe Item. Ele recebe os parametros definidos para a classe e é usado para inicializar os membros da classe quando um objeto da classe é criado;
+    /// @param id O identificador único do item.
+    /// @param name O nome do item.
+    /// @param price O preço do item.
+    /// @param status O status do item.
     Item(int id, std::string name, double price, bool status);
+    /// @brief Função para obter o nome do item.
+    /// @return O nome do item.
     std::string getName();
 
-    /// @brief retorna o preço de uma unidade daquele item
-    /// @param name nome do item buscado
+    /// @brief retorna o preço de uma unidade daquele item.
+    /// @param name nome do item buscado.
     double getUnitaryPrice();
 
+    /// @brief Função para obter o ID do item.
+    /// @return O ID do item.
     int getId();
 
+    /// @brief Função para obter o status do item.
+    /// @return O status do item.
     bool getStatus();
 
+    /// @brief Função para alterar o preço do item.
+    /// @param novo O novo preço a ser atribuído ao item.
     void changePrice(double novo);
 
+    /// @brief Função para alterar o status do item.
     void changeStatus();
 
 private:
--- ItemsStorage.hpp
+++ ItemsStorage.hpp
@@ -3,9 +3,21 @@
 class ItemsStorage : public StorageManager
 {
 public:
+    /// @brief Construtor responsável por inicializar a classe ItemsStorage com base no caminho do arquivo passado (Ponteiro como parametro).
+    /// @param file_path O caminho do arquivo a ser utilizado para armazenamento.
     ItemsStorage(char const *file_path);
+
+    /// @brief Destrutor da classe usado para liberar recursos alocados na memória.
     ~ItemsStorage();
+
+    /// @brief Método para escrever um item no armazenamento (substitui o método virtual da classe base).
     void write(const Item item) override;
+
+    /// @brief Método para ler dados do armazenamento.
+    /// @param data Os dados a serem lidos do armazenamento.
     void read(char const *data) override;
+
+    /// @brief Método para encontrar um item específico no armazenamento.
+    /// @param data Os dados a serem utilizados para encontrar o item.
     void findOne(char const *data) override;
 };
