bc63f98c18bf0ea79c3be7cf21236f1a3ca7ceb6
Naually <148272719+Naually@users.noreply.github.com>
2023-12-01 11:10:11 -0300

    Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -5,30 +5,36 @@
 class Item
 {
 public:
-    //construtor da classe Item. Ele recebe os parametros definidos para a classe 
-    //e é usado para inicializar os membros da classe quando um objeto da classe é criado;
+    
+    /// @brief Construtor da classe Item. Ele recebe os parametros definidos para a classe e é usado para inicializar os membros da classe quando um objeto da classe é criado;
+    /// @param id O identificador único do item.
+    /// @param name O nome do item.
+    /// @param price O preço do item.
+    /// @param status O status do item.
     Item(int id, std::string name, double price, bool status);
-    // Função usada para obter o nome do item;
+    /// @brief Função para obter o nome do item.
+    /// @return O nome do item.
     std::string getName();
 
-    /// @brief retorna o preço de uma unidade daquele item
-    /// @param name nome do item buscado
+    /// @brief retorna o preço de uma unidade daquele item.
+    /// @param name nome do item buscado.
     double getUnitaryPrice();
 
-    // Função para obter o ID do item;
+    /// @brief Função para obter o ID do item.
+    /// @return O ID do item.
     int getId();
 
-    // Função para obter o status do item;
+    /// @brief Função para obter o status do item.
+    /// @return O status do item.
     bool getStatus();
 
-    // Função para alterar o preço do item;
+    /// @brief Função para alterar o preço do item.
+    /// @param novo O novo preço a ser atribuído ao item.
     void changePrice(double novo);
 
-    // Função para alterar o status do item;
+    /// @brief Função para alterar o status do item.
     void changeStatus();
 
-// O modificador de acesso private, é usado para impedir e proteger os membros 
-//de serem modificados ou acessados diretamente de fora da classe. (Encapsulamento);
 private:
     int _id;
     std::string _name;
