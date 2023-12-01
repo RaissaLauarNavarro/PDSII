83eec1dd38f9006d81021694f40d9c2ba88a5e1f
Naually <148272719+Naually@users.noreply.github.com>
2023-12-01 10:05:02 -0300

    ItemsStorage.hpp

--- ItemsStorage.hpp
+++ ItemsStorage.hpp
@@ -3,9 +3,18 @@
 class ItemsStorage : public StorageManager
 {
 public:
+    // Construtor responsável por inicializar a classe ItemsStorage com base no caminho do arquivo passado (Ponteiro como parametro).
     ItemsStorage(char const *file_path);
+
+    // Destrutor da classe usado para liberar recursos alocados na memória;
     ~ItemsStorage();
+
+    // Método para escrever um item no armazenamento (substitui o método virtual da classe base);
     void write(const Item item) override;
+
+    //Método para ler dados do armazenamento;
     void read(char const *data) override;
+
+    //Método para encontrar um item específico no armazenamento;
     void findOne(char const *data) override;
 };
