d4cb6ee7512599d290bd5dff8be953c194bf2f6d
Naually <148272719+Naually@users.noreply.github.com>
2023-12-02 19:03:11 -0300

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -6,10 +6,18 @@
 class Player
 {
 public:
+    /// @brief Construtor padrão da classe Player.
     Player();
+    /// @brief Construtor parametrizado da classe Player.
+    /// @param name: O nome do jogador.
     Player(const std::string name);
+
+    /// @brief Função para testar a funcionalidade da classe Player.
     void functeste();
+    /// @brief Função para imprimir o inventário do jogador.
     void printInventory();
+    /// @brief Função para obter uma referência para o inventário do jogador.
+    /// @return Referência para o inventário do jogador.
     Inventory &getInventory();
     // // virtual ~StorageManager();
     // void write(const T &data);
