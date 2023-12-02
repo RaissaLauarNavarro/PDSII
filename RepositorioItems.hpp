200f6c2814ce17ccd48386b2ac03f95517d84a78
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-02 19:11:31 -0300

    Update RepositorioItems.hpp

--- RepositorioItems.hpp
+++ RepositorioItems.hpp
@@ -1,16 +1,22 @@
 #pragma once
 #include <set>
 
-class RepositorioItems
-{
+/// @brief Classe que representa um repositório de itens.
+class RepositorioItems {
+public:
+    /// @brief Construtor da classe RepositorioItems.
     RepositorioItems();
 
-    bool insert(Item item)
+    /// @brief Insere um item no repositório.
+    /// @param item O item a ser inserido.
+    /// @return Verdadeiro se a inserção for bem-sucedida, falso caso contrário.
+    bool insert(Item item);
 
-    bool list(std::set<Item> items)
+    /// @brief Lista os itens presentes no repositório.
+    /// @param items Conjunto onde os itens serão listados.
+    /// @return Verdadeiro se a lista for bem-sucedida, falso caso contrário.
+    bool list(std::set<Item> items);
 
-    private:
-    {
-        std::set<Item> _repositorioItems;
-    }
-}
+private:
+    std::set<Item> _repositorioItems; ///< Conjunto que armazena os itens no repositório.
+};
