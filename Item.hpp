a91ba42d4da35350dc7a6ed198a1c692cf219913
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-02 19:04:11 -0300

    Update Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -2,27 +2,42 @@
 #include <map>
 #include <string>
 
-class Item
-{
+/// @brief Classe que representa um item no sistema.
+class Item {
 public:
+    /// @brief Construtor da classe Item.
+    /// @param id Identificador único do item.
+    /// @param name Nome do item.
+    /// @param price Preço do item.
+    /// @param status Status do item.
     Item(int id, std::string name, double price, bool status);
+
+    /// @brief Obtém o nome do item.
+    /// @return Nome do item.
     std::string getName();
 
-    /// @brief retorna o preço de uma unidade daquele item
-    /// @param name nome do item buscado
+    /// @brief Obtém o preço unitário do item.
+    /// @return Preço unitário do item.
     double getUnitaryPrice();
 
+    /// @brief Obtém o identificador único do item.
+    /// @return Identificador único do item.
     int getId();
 
+    /// @brief Obtém o status do item.
+    /// @return Status do item.
     bool getStatus();
 
+    /// @brief Altera o preço do item.
+    /// @param novo Novo preço do item.
     void changePrice(double novo);
 
+    /// @brief Altera o status do item.
     void changeStatus();
 
 private:
-    int _id;
-    std::string _name;
-    double _price;
-    bool _status;
+    int _id; ///< Identificador único do item.
+    std::string _name; ///< Nome do item.
+    double _price; ///< Preço do item.
+    bool _status; ///< Status do item.
 };
