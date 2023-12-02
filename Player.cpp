5a0a06319fc02f92aae11be85cfd870a2af628b1
Naually <148272719+Naually@users.noreply.github.com>
2023-12-02 19:18:21 -0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,21 +4,18 @@
 #include "Item.hpp"
 #include <iostream>
 
-/// @brief Construtor padrão da classe Player.
-Player::Player() : _name(""), _nivel(0), _inventario(0){};
 
-/// @brief Construtor parametrizado da classe Player.
-/// @param name: O nome do jogador.
+Player::Player() : _name(""), _nivel(0), _inventario(0){};
 Player::Player(const std::string name) : _name(name), _nivel(0), _inventario(10){};
 
-/// @brief Função para testar a funcionalidade da classe Player.
+
 void Player::functeste()
 {
     Item ITEMTESTE(0, "Item teste 1", 10.3, true);
     _inventario.insert(ITEMTESTE.getId(), ITEMTESTE);
     _inventario.list();
 }
-/// @brief Função para imprimir o inventário do jogador.
+
 void Player::printInventory()
 {
     _inventario.list();
