fb1d1edc001548e6ea500baa194170494fa0a2cc
Naually <148272719+Naually@users.noreply.github.com>
2023-12-04 19:10:09 -0300

    Update LojaDeItens.hpp

--- LojaDeItens.hpp
+++ LojaDeItens.hpp
@@ -5,20 +5,18 @@
 #include "Player.hpp"
 #include "Inventory.hpp"
 
-/// @brief Classe que representa uma loja de itens no jogo
+
 class LojaDeItens
 {
 public:
-    /// @brief Construtor da loja de itens
-    /// @param player Ponteiro para o jogador que irá interagir com a loja
-    /// @param inventory Ponteiro para o inventário do jogador
+
     LojaDeItens(Player* player, Inventory* inventory, Item* item);
 
-    /// @brief Exibe os itens disponíveis na loja e permite ao jogador comprar ou vender
+    
     void exibirItens();
-    ///@brief Realiza a compra dos itens na loja
+    
     void LojaDeItens::comprarItem();
-    ///@brief Vende os itens do inventário do jogador
+    
     void LojaDeItens::venderItem();
 
 private:
