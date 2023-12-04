be927aaf2ec251bb5f4bb563e082804465a298e4
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-04 07:49:00 -0300

    Update StorageManager.hpp

--- StorageManager.hpp
+++ StorageManager.hpp
@@ -3,25 +3,38 @@
 #include <string>
 #include <sstream>
 
-/// @brief Classe modelo para gerenciamento de armazenamento.
-/// @tparam T Tipo de dados a ser armazenado.
+/**
+ * @file
+ * @brief Contém a definição da classe template StorageManager.
+ */
+
+/**
+ * @brief Classe modelo para gerenciamento de armazenamento.
+ * @tparam T Tipo de dados a ser armazenado.
+ */
 template <typename T>
 class StorageManager {
 public:
-    /// @brief Construtor da classe StorageManager.
-    /// @param file_path Caminho do arquivo de armazenamento.
+    /**
+     * @brief Construtor da classe StorageManager.
+     * @param file_path Caminho do arquivo de armazenamento.
+     */
     StorageManager(char const *file_path);
 
     // Métodos comentados para remoção temporária
     // virtual ~StorageManager();
     
-    /// @brief Escreve dados no arquivo de armazenamento.
-    /// @param data Os dados a serem escritos.
+    /**
+     * @brief Escreve dados no arquivo de armazenamento.
+     * @param data Os dados a serem escritos.
+     */
     void write(const T &data);
     
-    /// @brief Lê dados do arquivo de armazenamento.
-    /// @param data Os dados a serem lidos.
-    /// @param totalRegistros O número total de registros a serem lidos.
+    /**
+     * @brief Lê dados do arquivo de armazenamento.
+     * @param data Os dados a serem lidos.
+     * @param totalRegistros O número total de registros a serem lidos.
+     */
     void read(const T &data, int totalRegistros);
     
     // Métodos comentados para remoção temporária
