350ec311f09d44f8fcdea7ac7bb1479e25528e79
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-04 07:58:17 -0300

    Update ItemsStorage.hpp

--- ItemsStorage.hpp
+++ ItemsStorage.hpp
@@ -1,25 +1,43 @@
 #pragma once
 #include "StorageManager.hpp"
 
-/// @brief Classe que gerencia o armazenamento de itens.
+/**
+ * @file
+ * @brief Contém a definição da classe ItemsStorage, uma subclasse de StorageManager.
+ */
+
+/**
+ * @class ItemsStorage
+ * @brief Gerencia o armazenamento de itens.
+ */
 class ItemsStorage : public StorageManager {
 public:
-    /// @brief Construtor da classe ItemsStorage.
-    /// @param file_path Caminho do arquivo de armazenamento.
+    /**
+     * @brief Construtor da classe ItemsStorage.
+     * @param file_path Caminho do arquivo de armazenamento.
+     */
     ItemsStorage(char const *file_path);
 
-    /// @brief Destrutor da classe ItemsStorage.
+    /**
+     * @brief Destrutor da classe ItemsStorage.
+     */
     ~ItemsStorage();
 
-    /// @brief Escreve um item no armazenamento.
-    /// @param item O item a ser escrito.
+    /**
+     * @brief Escreve um item no armazenamento.
+     * @param item O item a ser escrito.
+     */
     void write(const Item item) override;
 
-    /// @brief Lê dados do armazenamento.
-    /// @param data Os dados a serem lidos.
+    /**
+     * @brief Lê dados do armazenamento.
+     * @param data Os dados a serem lidos.
+     */
     void read(char const *data) override;
 
-    /// @brief Encontra um item no armazenamento.
-    /// @param data Os dados do item a ser encontrado.
+    /**
+     * @brief Encontra um item no armazenamento.
+     * @param data Os dados do item a ser encontrado.
+     */
     void findOne(char const *data) override;
 };
