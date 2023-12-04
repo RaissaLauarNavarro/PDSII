8607898d79873ee10a2fff29c7721ba4992c4e6a
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-04 07:29:12 -0300

    Update Farmer.hpp

--- Farmer.hpp
+++ Farmer.hpp
@@ -2,16 +2,29 @@
 #include "Item.hpp"
 #include "Inventory.hpp"
 
+/**
+ * @file
+ * @brief Contém a definição da classe Farmer.
+ */
+
+/**
+ * @class Farmer
+ * @brief Representa um fazendeiro no jogo.
+ */
 class Farmer
 {
-    public:
-        /// @brief Substitui o item semente por trigo
-        /// @param quantidade número de trigos a serem obtidos
-        /// @param inventario ponteiro do invetário do jogador com seus itens
-        void getWheat(int quantidade, Inventory* inventario);
+public:
+    /**
+     * @brief Substitui o item semente por trigo.
+     * @param quantidade Número de trigos a serem obtidos.
+     * @param inventario Ponteiro para o inventário do jogador com seus itens.
+     */
+    void getWheat(int quantidade, Inventory* inventario);
 
-        /// @brief Substitui o item semente por cenoura
-        /// @param quantidade número de cenouras a serem obtidas
-        /// @param inventario ponteiro do invetário do jogador com seus itens
-        void getCarrot(int quantidade, Inventory* inventario);
+    /**
+     * @brief Substitui o item semente por cenoura.
+     * @param quantidade Número de cenouras a serem obtidas.
+     * @param inventario Ponteiro para o inventário do jogador com seus itens.
+     */
+    void getCarrot(int quantidade, Inventory* inventario);
 };
