222e530ac7efd4c09654d87a19c404dc175b198a
Pierryjean <89036905+Pierryjean@users.noreply.github.com>
2023-12-04 19:13:04 -0300

    Create LojaDeItens.hpp

---
+++ LojaDeItens.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <map>
+#include <string>
+#include "Item.hpp"
+#include "Player.hpp"
+#include "Inventory.hpp"
+
+/// @brief Classe que representa uma loja de itens no jogo
+class LojaDeItens
+{
+public:
+    /// @brief Construtor da loja de itens
+    /// @param player Ponteiro para o jogador que irá interagir com a loja
+    /// @param inventory Ponteiro para o inventário do jogador
+    LojaDeItens(Player* player, Inventory* inventory, Item* item);
+
+    /// @brief Exibe os itens disponíveis na loja e permite ao jogador comprar ou vender
+    void exibirItens();
+    ///@brief Realiza a compra dos itens na loja
+    void LojaDeItens::comprarItem();
+    ///@brief Vende os itens do inventário do jogador
+    void LojaDeItens::venderItem();
+
+private:
+    Player* _player;
+    Inventory* _inventory;
+    Item* _items;
+};
