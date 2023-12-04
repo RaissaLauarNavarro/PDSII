96ae0cfb5825c025c5c36a9b08993ab154602f25
Pierryjean <89036905+Pierryjean@users.noreply.github.com>
2023-12-04 17:26:59 -0300

    Update LojaDeItens.cpp

--- LojaDeItens.cpp
+++ LojaDeItens.cpp
@@ -1,10 +1,17 @@
 #include "LojaDeItens.hpp"
 #include <iostream>
 
-LojaDeItens::LojaDeItens(Player* player, Inventory* inventory) : _player(player), _inventory(inventory) {}
+LojaDeItens::LojaDeItens(Player* player, Inventory* inventory, std::vector<Item>* items) 
+    : _player(player), _inventory(inventory), _items(items) {}
 
 void LojaDeItens::exibirItens()
 {
+    std::cout << "Itens da Loja:\n";
+    for (const auto& item : *_items) {
+        std::cout << item._id << " - " << item._name << " - $" << item._price << "\n";
+    }
+    std::cout << "\n";
+
     std::cout << "Escolha uma opção:" << std::endl;
     std::cout << "1. Comprar Item" << std::endl;
     std::cout << "2. Vender Item" << std::endl;
@@ -15,12 +22,24 @@ void LojaDeItens::exibirItens()
     switch (opcao)
     {
     case 1:
-        _inventory->list(); // Chama a função list do inventário para exibir os itens
+        comprarItem();
         break;
     case 2:
-        _inventory->list(); // Chama a função list do inventário para exibir os itens
+        venderItem();
         break;
     default:
         std::cout << "Opção inválida!" << std::endl;
     }
+}
+
+void LojaDeItens::comprarItem()
+{
+    _inventory->list(); // Chama a função list do inventário para exibir os itens
+    // Adicione lógica para compra de item aqui
+}
+
+void LojaDeItens::venderItem()
+{
+    _inventory->list(); // Chama a função list do inventário para exibir os itens
+    // Adicione lógica para venda de item aqui
 }
