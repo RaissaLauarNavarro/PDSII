912f2855ecb73883331d26ef0f51dfe2fdd76485
Emanuelle AMaral <102487473+manuamaral@users.noreply.github.com>
2023-12-04 07:57:23 -0300

    Update Livestocker.hpp

--- Livestocker.hpp
+++ Livestocker.hpp
@@ -2,16 +2,29 @@
 #include "Item.hpp"
 #include "Inventory.hpp"
 
+/**
+ * @file
+ * @brief Contém a definição da classe Livestocker.
+ */
+
+/**
+ * @class Livestocker
+ * @brief Representa um criador de animais no jogo.
+ */
 class Livestocker
 {
-    public:
-        /// @brief Substitui o item cenoura por bacon
-        /// @param quantidade número de bacons a serem obtidos
-        /// @param player ponteiro o jogador
-        void getBacon(int quantidade, Player* p);
+public:
+    /**
+     * @brief Substitui o item cenoura por bacon.
+     * @param quantidade Número de bacons a serem obtidos.
+     * @param player Ponteiro para o jogador.
+     */
+    void getBacon(int quantidade, Player* player);
 
-        /// @brief Substitui o trigo semente por ovo
-        /// @param quantidade número de ovos a serem obtidos
-        /// @param player ponteiro o jogador
-        void getEgg(int quantidade, Player* p);
+    /**
+     * @brief Substitui o item trigo semente por ovo.
+     * @param quantidade Número de ovos a serem obtidos.
+     * @param player Ponteiro para o jogador.
+     */
+    void getEgg(int quantidade, Player* player);
 };
